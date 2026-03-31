e807e4238d3348b74e39eab9d450b277ba5f7ca0
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-03-31 12:13:46 -0500

    Implementing map component and city

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const App = () => {
   const [cities, setCities] = useState([]);
@@ -51,11 +53,12 @@ const App = () => {
               path="cities"
               element={<CityList cities={cities} loading={loading} />}
             />
+            <Route path="cities/:id" element={<City />}></Route>
             <Route
               path="countries"
               element={<CountryList loading={loading} cities={cities} />}
             />
-            <Route path="form" element={<p>Form</p>} />
+            <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
